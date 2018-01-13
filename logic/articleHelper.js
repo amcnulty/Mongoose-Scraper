dbf2d585ee9f8fc866f8793d95bf5062b939a54d
amcnulty <amcnulty88@swbell.net>
2018-01-12 20:30:09 -0800

    Unique Articles
    Only new articles get added to the DB.
    Added a default value of false to saved property in article schema.

--- logic/articleHelper.js
+++ logic/articleHelper.js
@@ -5,13 +5,29 @@ const db = require('../config/dbSetup');
 const first = values => values[0];
 const rest = values => values.slice(1);
 
+const articleExists = function(record, cb) {
+  db.Article.findOne({ 'heading': record.heading }, function(err, record) {
+    if (err) console.log(err);
+    else if (record) cb(true);
+    else cb(false);
+  });
+}
+
 const insertRecords = function(articleData, cb) {
   const record = first(articleData);
   const remaining = rest(articleData);
-  db.Article.create(record, function(err) {
-    if (err) cb(err);
-    else if (remaining.length === 0) cb(null);
-    else insertRecords(remaining, cb);
+  articleExists(record, function(exists) {
+    if (!exists) {
+      db.Article.create(record, function(err) {
+        if (err) cb(err);
+        else if (remaining.length === 0) cb(null);
+        else insertRecords(remaining, cb);
+      });
+    }
+    else {
+      if (remaining.length === 0) cb(null);
+      else insertRecords(remaining, cb);
+    }
   });
 }
 
