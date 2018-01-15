4b047d8529bfd6120c95ae545093c71ac44d3ea2
amcnulty <amcnulty88@swbell.net>
2018-01-15 06:29:04 -0800

    Header
    Added a header to the page this is temporary and not working at the moment.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var hbs = require('hbs');
 
 var index = require('./routes/index');
 
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
